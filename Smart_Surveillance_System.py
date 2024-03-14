27680b75a840e33888b37b353c1d958a52e47f1b
ShuklaArpit4554 <arpitshukla7002@gmail.com>
2024-03-14 09:19:49 +0530

    update 1.py
    import libraries

---
+++ Smart_Surveillance_System.py
@@ -0,0 +1,10 @@
+#in this program we are using six libraries of python programing
+
+#import necesseary libraries
+
+import cv2      #open cv library for take input from camera
+from datetime import datetime   #for current date and time
+import time     #to work with time related  functions
+from twilio.rest import Client  #Twilio API for sending SMS
+from plyer import notification  #plyer library for desktop notification
+import pygame       #pygame library for playing sound
