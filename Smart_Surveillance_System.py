a8735b52d80b86728b0e32b17ace8a4fbd42c0a9
ShuklaArpit4554 <arpitshukla7002@gmail.com>
2024-03-14 10:10:19 +0530

    update 2
    Twilio API

--- Smart_Surveillance_System.py
+++ Smart_Surveillance_System.py
@@ -8,3 +8,12 @@ import time     #to work with time related  functions
 from twilio.rest import Client  #Twilio API for sending SMS
 from plyer import notification  #plyer library for desktop notification
 import pygame       #pygame library for playing sound
+
+#Twilio credentials(replace with your own values) 
+account_sid = 'Enter your account sid'
+auth_token = 'Enter your auth token'
+twilio_phone_number = 'Enter twilio phone number'
+your_phone_number = 'Enter your phone number'
+
+#initialize Twilio client
+client = Client(account_sid, auth_token)
