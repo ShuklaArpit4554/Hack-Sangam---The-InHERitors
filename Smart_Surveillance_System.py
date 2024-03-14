3f53d3b06164347a625a0a94152095598c9b6226
ShuklaArpit4554 <arpitshukla7002@gmail.com>
2024-03-14 11:03:04 +0530

    update 3
    initialize time variable

--- Smart_Surveillance_System.py
+++ Smart_Surveillance_System.py
@@ -17,3 +17,35 @@ your_phone_number = 'Enter your phone number'
 
 #initialize Twilio client
 client = Client(account_sid, auth_token)
+
+#Load The  Haar cascade classifier for face detection
+
+#NOTE -> The Haar Cascade classifier is like a smart tool in Python that can quickly find faces in pictures or videos.-
+# It has been trained to recognize patterns that make up a face, such as eyes, nose,-
+# and mouth. When you use this tool, it scans through images, and when it thinks it has found a face,-
+# it lets your program know. It's like giving your computer the ability to see and point out faces!
+
+face_cascade = cv2.CascadeClassifier("D:\Hack-Sangam---The-InHERitors\haarcascade_frontalface_default.xml")
+
+#open a video capture object , here we are using the default camera(camera index 0)
+video = cv2.VideoCapture(1)
+
+# Define intervals for different actions
+photo_capture_interval = 4       # Capture interval in seconds
+notification_interval = 10       # Notification interval in seconds
+sms_interval = 25                 # SMS interval in seconds
+siren_interval = 3                # Siren interval in seconds
+
+#initialize time variables
+start_time = time.time()
+notification_start_time = time.time()
+sms_start_time = time.time()
+siren_start_time = time.time()
+exact_time = ""         # Initialize exact_time outside the loop
+
+# Initialize Pygame for sound
+pygame.mixer.init()
+siren_sound = pygame.mixer.Sound(r"D:\Hack-Sangam---The-InHERitors\siren.wav")
+
+
+
